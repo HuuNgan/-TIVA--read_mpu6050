c2acc15b8fbc1327616410ad7875f75cdad1ec45
HuuNgan <ngan.luongbkdev@hcmut.edu.vn>
2019-05-04 14:26:11 +0700

    add new files

---
+++ Debug/main.d
@@ -0,0 +1,25 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/sys/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/machine/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/sys/_stdint.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/hw_mpu6050.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/i2cm_drv.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/mpu6050.h
+
+../main.c: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.5.LTS/include/sys/_stdint.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/hw_mpu6050.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/i2cm_drv.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/mpu6050.h: 
